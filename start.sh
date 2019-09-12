442e0e251cb3d682bb9ff3851f260126ac9836a2
wuun <1758009040@qq.com>
2019-09-12 15:41:23 +0800

    strt

---
+++ start.sh
@@ -0,0 +1,10 @@
+export GO111MODULE=172.17.25.155
+export GOPROXY=https://goproxy.io
+export MYSQL_HOST=172.17.25.155
+export MYSQL_PWD=wudun123
+export MYSQL_PORT=8949
+export MYSQL_DB_NAME=blog
+export MYSQL_USER=root
+export WEBSITE_PASS_WORD=6862038abc
+export WEB_ADDR=0.0.0.0:80
+export WEB_SECRET=wuun
